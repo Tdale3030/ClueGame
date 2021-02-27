72751d4c068cc086b724a613b37e1e258fe88fd3
brodyclark <brodyclark@mymail.mines.edu>
2021-02-26 18:20:12 -0700

    making edits

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,11 +1,31 @@
 package experiment;
+import java.util.*;
 
 public class TestBoardCell {
-	private int col, row;
+    private int col, row;
 
-	public TestBoardCell(int col, int row) {
-		super();
-		this.col = col;
-		this.row = row;
-	}
+    public TestBoardCell(int col, int row) {
+        super();
+        this.col = col;
+        this.row = row;
+    }
+
+    void addAdjacency( TestBoardCell cell ) {
+
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+
+    Set<TestBoardCell> getAdjList(){
+
+        return 
+
+    };
 }
